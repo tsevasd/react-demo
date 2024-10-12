3dfe42cc4c99793581b47f4c48dafa4e6e5f2132
Dimosthenis <119672831+tsevasd@users.noreply.github.com>
2024-10-12 20:47:01 +0300

    Add files via upload

--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -27,69 +27,55 @@ export default function Settings({t, handleChangeLanguage}){
         <div className="px-6 pb-4 max-w-3xl">
             <h1 className="text-2xl text-titlecolor mb-4">{t('header.menu.settings')}</h1>
             <h3 className="pb-4 border-b border-darkcolor mb-4"><span className="text-secondary">{theme.name}</span> {t('settings.theme')} &amp; {t('settings.user')} <span className="text-secondary">{user.name}</span> {t('header.menu.settings')}</h3>
-            <table className="w-full">
-                <tbody>
-                <tr>
-                    <td className="pr-6 py-2 font-bold w-1/2">{t('settings.themeName')}</td>
-                    <td className="py-2 w-1/2">
-                        <input type="text"
-                            className="w-full px-4 py-2 text-black rounded-full"
-                            value={themeName}
-                            onChange={handleThemeName}
-                        />
-                    </td>
-                </tr>
-                <tr>
-                    <td className="pr-6 py-2 font-bold">{t('settings.username')}</td>
-                    <td className="py-2">
-                        <input type="text"
-                            className="w-full px-4 py-2 text-black rounded-full"
-                            value={userName}
-                            onChange={handleUserName}
-                        />
-                    </td>
-                </tr>
-                <tr>
-                    <td className="pr-6 py-2 font-bold">{t('settings.languages')}</td>
-                    <td className="py-2">
-                        <div className="flex gap-2">
-                            {theme.languages.map(language => (
-                                <a
-                                    key={language}
-                                    className={`flex w-12 h-12 items-center justify-center rounded-full uppercase transition-colors ${language === theme.lang ? 'bg-primary text-primary-variant-light' : 'border-2 border-primary hover:bg-area/10'}`}
-                                    onClick={() => { handleChangeLanguage(language, theme) }}
-                                >{language}</a>
-                            ))}
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td className="pr-6 py-2 font-bold">{t('settings.currentTheme')}</td>
-                    <td className="py-2">
-                        <div className="px-4 py-2 border-2 border-primary rounded-full uppercase">
-                            {theme.theme}
-                        </div>
-                    </td>
-                </tr>
-                <tr>
-                    <td className="pr-6 py-2 font-bold">{t('settings.changeTheme')}</td>
-                    <td className="py-2">
-                        <button className="block w-full h-12 p-2 rounded-full bg-area/10 hover:bg-area/20 transition-colors overflow-hidden whitespace-nowrap text-left" onClick={toggleTheme}>
-                            { theme.theme === "dark" ? <IconSun iconClassName="w-8 h-8 stroke-secondary inline"></IconSun> : <IconMoon iconClassName="w-8 h-8 stroke-primary-variant inline"></IconMoon> }
-                            <span className="ml-3 align-middle">{ theme.theme === "dark" ? t('header.menu.changeThemeLight') : t('header.menu.changeThemeDark') }</span>
-                        </button>
-                    </td>
-                </tr>
-                <tr>
-                    <td className="pr-6 py-2 font-bold">{t('settings.sidebarStatus')}</td>
-                    <td className="py-2">
-                        <div className="px-4 py-2 border-2 border-primary rounded-full uppercase">
-                            {theme.sidebar}
-                        </div>
-                    </td>
-                </tr>
-                </tbody>
-            </table>
+            <div className="grid grid-cols-2">
+                <div className="pr-6 py-2 font-bold">{t('settings.themeName')}</div>
+                <div className="py-2">
+                    <input type="text"
+                        className="w-full px-4 py-2 text-black rounded-full"
+                        value={themeName}
+                        onChange={handleThemeName}
+                    />
+                </div>
+                <div className="pr-6 py-2 font-bold">{t('settings.username')}</div>
+                <div className="py-2">
+                    <input type="text"
+                        className="w-full px-4 py-2 text-black rounded-full"
+                        value={userName}
+                        onChange={handleUserName}
+                    />
+                </div>
+                <div className="pr-6 py-2 font-bold">{t('settings.languages')}</div>
+                <div className="py-2">
+                    <div className="flex gap-2">
+                        {theme.languages.map(language => (
+                            <a
+                                key={language}
+                                className={`flex w-12 h-12 items-center justify-center rounded-full uppercase transition-colors ${language === theme.lang ? 'bg-primary text-primary-variant-light' : 'border-2 border-primary hover:bg-area/10'}`}
+                                onClick={() => { handleChangeLanguage(language, theme) }}
+                            >{language}</a>
+                        ))}
+                    </div>
+                </div>
+                <div className="pr-6 py-2 font-bold">{t('settings.currentTheme')}</div>
+                <div className="py-2">
+                    <div className="px-4 py-2 border-2 border-primary rounded-full uppercase">
+                        {theme.theme}
+                    </div>
+                </div>
+                <div className="pr-6 py-2 font-bold">{t('settings.changeTheme')}</div>
+                <div className="py-2">
+                    <button className="block w-full h-12 p-2 rounded-full bg-area/10 hover:bg-area/20 transition-colors overflow-hidden whitespace-nowrap text-left" onClick={toggleTheme}>
+                        { theme.theme === "dark" ? <IconSun iconClassName="w-8 h-8 stroke-secondary inline"></IconSun> : <IconMoon iconClassName="w-8 h-8 stroke-primary-variant inline"></IconMoon> }
+                        <span className="ml-3 align-middle">{ theme.theme === "dark" ? t('header.menu.changeThemeLight') : t('header.menu.changeThemeDark') }</span>
+                    </button>
+                </div>
+                <div className="pr-6 py-2 font-bold">{t('settings.sidebarStatus')}</div>
+                <div className="py-2">
+                    <div className="px-4 py-2 border-2 border-primary rounded-full uppercase">
+                        {theme.sidebar}
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
